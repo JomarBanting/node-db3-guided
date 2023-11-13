f3d3e29bd482667efc92bf32c026cdd1e7b88e1c
Jomar Banting <shadowlover.05@gmail.com>
2023-11-12 19:31:50 -0500

    Finished guided project

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const server = express()
 
 server.use(express.json())
 server.use('/api/users', UserRouter)
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message })
 })
--- api/users/user-model.js
+++ api/users/user-model.js
@@ -8,8 +8,16 @@ module.exports = {
   remove
 }
 
-function findPosts(user_id) {
+async function findPosts(user_id) {
+  const result = await db("posts").select("posts.id as post_id", "posts.contents as contents", "users.username as username")
+                  .join("users", "posts.user_id", "=" ,"users.id").where("users.id", user_id);
+  return result;
   /*
+    raw = 
+    select posts.id as post_id, posts.contents, users.username
+    from posts join users
+    on posts.user_id = users.id;
+
     Implement so it resolves this structure:
 
     [
@@ -25,6 +33,10 @@ function findPosts(user_id) {
 
 function find() {
   return db('users')
+  .leftJoin("posts", "posts.id", "users.id")
+  .groupBy("users.id")
+  .select("users.id as user_id", "users.username")
+  .count("posts.id as post_count");
   /*
     Improve so it resolves this structure:
 
@@ -44,8 +56,18 @@ function find() {
   */
 }
 
-function findById(id) {
-  return db('users').where({ id }).first()
+async function findById(id) {
+  const rows = await db('users')
+  .leftJoin("posts", "posts.id", "users.id")
+  .select("users.id as user_id", "users.username")
+  .where("users.id", id);
+
+  const contents = await db("posts")
+  .select("posts.id as post_id","posts.contents")
+  .where("posts.user_id", id);
+
+  rows[0]["posts"] = contents;
+  return rows;
   /*
     Improve so it resolves this structure:
 
